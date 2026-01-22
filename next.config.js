0e32899d5e95e1db5bd34556920c01b1dfeb082b
Eric Larssen <elarssen91@gmail.com>
2026-01-21 22:23:49 -0600

    Fix Next.js 15 compatibility issues

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   output: 'export',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
